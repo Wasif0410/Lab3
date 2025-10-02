2e19ac8edebfec5f8f835cd32b4b6ab7a8b90df4
Wasif0410 <wasifsaeed0410@gmail.com>
2025-10-01 22:53:41 -0400

    Second Message

---
+++ Jfile.js
@@ -0,0 +1,4 @@
+console.log("working on source code using Git & GitHub");
+console.log("code sample for A3 in JavaScript"); 
+Change1
+check1
---
+++ Pfile.py
@@ -0,0 +1,4 @@
+print("working on source code using Git & GitHub");
+print("code sample for A3 in Python");
+Change2
+Check2
